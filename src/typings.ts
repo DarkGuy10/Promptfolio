b359d3d854e154230d148d940c09cae85a38ab9a
DarkGuy10 <HarperBurn@protonmail.ch>
2022-03-30 13:34:40 +0900

    chore: typescript rewrite

---
+++ src/typings.ts
@@ -0,0 +1,32 @@
+type AppState = {
+	record: Array<{ command: string; output: JSX.Element }>
+	commands: Map<string, Command>
+	projectDataLoaded: boolean
+	userDataLoaded: boolean
+	projectData?: any
+	userData?: any
+}
+
+type InputManagerState = {
+	value: string
+	suggestedValue: string
+}
+
+type ListElementProps = {
+	icon: string
+	name: string
+	link?: string
+	description: string
+	help?: boolean
+}
+
+type Command = {
+	name: string
+	icon: string
+	description: string
+	execute(app: any): void
+}
+
+type Commands = Map<string, Command>
+
+export { AppState, InputManagerState, ListElementProps, Command, Commands }

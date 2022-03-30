31ea68828313fb6618d13598939aecd902014400
DarkGuy10 <DarkGuy10@users.noreply.github.com>
2022-03-30 05:40:17 +0000

    style(prettier): fix linting

--- src/InputManager/InputManager.tsx
+++ src/InputManager/InputManager.tsx
@@ -1,91 +1,94 @@
-import React, { Component, RefObject, createRef } from "react";
-import commands from "../commands/commands";
-import { github_username } from "../config";
-import { InputManagerState } from "../typings";
-import styles from "./InputManager.module.css";
+import React, { Component, RefObject, createRef } from 'react'
+import commands from '../commands/commands'
+import { github_username } from '../config'
+import { InputManagerState } from '../typings'
+import styles from './InputManager.module.css'
 
 class InputManager extends Component<
-  { handleExecute: (commandName: string) => void },
-  InputManagerState
+	{ handleExecute: (commandName: string) => void },
+	InputManagerState
 > {
-  inputRef: RefObject<any>;
-  updateInputField: (value?: string) => void;
+	inputRef: RefObject<any>
+	updateInputField: (value?: string) => void
 
-  constructor(props: { handleExecute: (commandName: string) => void }) {
-    super(props);
-    this.state = {
-      value: "",
-      suggestedValue: "",
-    };
-    this.inputRef = createRef();
-    this.updateInputField = (value = "") => {
-      this.inputRef.current.value = value;
-    };
-  }
-  render() {
-    const { value } = this.state;
-    return (
-      <div className={styles.inputWrapper}>
-        <span className={styles.promptPrefix}>
-          <span>{github_username}</span>@<span>promptfolio:</span>
-          ~$&nbsp;
-        </span>
-        <form
-          action="#"
-          onSubmit={(event) => {
-            event.preventDefault();
-            this.setState({ value: "", suggestedValue: "" });
-            this.updateInputField();
-            this.props.handleExecute(value);
-          }}
-          className={styles.inputForm}
-        >
-          <span className={styles.suggested}>{this.state.suggestedValue}</span>
-          <input
-            className={`${styles.input} ${
-              commands.has(value) ? styles.validCommand : ""
-            }`}
-            spellCheck={false}
-            placeholder="confused? type help and hit enter to get started!"
-            ref={this.inputRef}
-            autoFocus
-            onKeyDown={(event) => {
-              const { value, suggestedValue } = this.state;
-              if (
-                (event.key === "Tab" || event.key === "ArrowRight") &&
-                suggestedValue
-              ) {
-                event.preventDefault();
-                const newValue = value + suggestedValue.trim();
-                this.updateInputField(newValue);
-                this.setState({
-                  value: newValue,
-                  suggestedValue: "",
-                });
-              }
-            }}
-            onInput={(event) => {
-              const target = event.target as HTMLInputElement;
-              const value = target.value.toLowerCase().trim();
-              let suggestedValue = "";
-              if (value)
-                for (const cmd of commands.values())
-                  if (cmd.name.startsWith(value)) {
-                    suggestedValue =
-                      " ".repeat(value.length) +
-                      cmd.name.substring(value.length);
-                    break;
-                  }
-              this.setState({
-                value: value,
-                suggestedValue: suggestedValue,
-              });
-            }}
-          />
-        </form>
-      </div>
-    );
-  }
+	constructor(props: { handleExecute: (commandName: string) => void }) {
+		super(props)
+		this.state = {
+			value: '',
+			suggestedValue: '',
+		}
+		this.inputRef = createRef()
+		this.updateInputField = (value = '') => {
+			this.inputRef.current.value = value
+		}
+	}
+	render() {
+		const { value } = this.state
+		return (
+			<div className={styles.inputWrapper}>
+				<span className={styles.promptPrefix}>
+					<span>{github_username}</span>@<span>promptfolio:</span>
+					~$&nbsp;
+				</span>
+				<form
+					action="#"
+					onSubmit={event => {
+						event.preventDefault()
+						this.setState({ value: '', suggestedValue: '' })
+						this.updateInputField()
+						this.props.handleExecute(value)
+					}}
+					className={styles.inputForm}
+				>
+					<span className={styles.suggested}>
+						{this.state.suggestedValue}
+					</span>
+					<input
+						className={`${styles.input} ${
+							commands.has(value) ? styles.validCommand : ''
+						}`}
+						spellCheck={false}
+						placeholder="confused? type help and hit enter to get started!"
+						ref={this.inputRef}
+						autoFocus
+						onKeyDown={event => {
+							const { value, suggestedValue } = this.state
+							if (
+								(event.key === 'Tab' ||
+									event.key === 'ArrowRight') &&
+								suggestedValue
+							) {
+								event.preventDefault()
+								const newValue = value + suggestedValue.trim()
+								this.updateInputField(newValue)
+								this.setState({
+									value: newValue,
+									suggestedValue: '',
+								})
+							}
+						}}
+						onInput={event => {
+							const target = event.target as HTMLInputElement
+							const value = target.value.toLowerCase().trim()
+							let suggestedValue = ''
+							if (value)
+								for (const cmd of commands.values())
+									if (cmd.name.startsWith(value)) {
+										suggestedValue =
+											' '.repeat(value.length) +
+											cmd.name.substring(value.length)
+										break
+									}
+							this.setState({
+								value: value,
+								suggestedValue: suggestedValue,
+							})
+						}}
+					/>
+				</form>
+			</div>
+		)
+	}
 }
 
-export default InputManager;
+export default InputManager
